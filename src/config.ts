05c26d4c7fc27b1a55d68e3b915d306fb821ab20
Nicolas Molina Monroy <hi@nicobytes.com>
2022-05-16 10:54:52 -0400

    :mag: Change open graph

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const KNOWN_LANGUAGES = {
 };
 
 // Uncomment this to add an "Edit this page" button to every page of documentation.
-export const GITHUB_EDIT_URL = `https://github.com/platzi/fake-api-site/blob/master/`;
+export const GITHUB_EDIT_URL = `https://github.com/platzilabs/fake-api-site/blob/master/`;
 
 // Uncomment this to add an "Join our Community" button to every page of documentation.
 // export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
